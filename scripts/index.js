7a9fcd464b1947d7c204c5bd1cd4c82ae9dc51e8
ivnpotapov <iojikov@mail.ru>
2022-02-06 17:05:33 +0700

    sprint 5 v 1.0

--- scripts/index.js
+++ scripts/index.js
@@ -1,35 +1,141 @@
-const profileButtonEdit = document.querySelector('.profile__button-edit'),
-  popup = document.querySelector('.popup'),
-  form = document.querySelector('.popup__form'),
-  profileName = document.querySelector('.profile__name-text'),
-  profileJob = document.querySelector('.profile__job'),
-  inputName = document.querySelector('.popup__input[name="profile-name"]'),
-  inputJob = document.querySelector('.popup__input[name="profile-job"]'),
-  popupClose = document.querySelector('.popup__close-icon')
+const initialCards = [
+  {
+    cardTitle: 'Архыз',
+    cardLink: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
+  },
+  {
+    cardTitle: 'Челябинская область',
+    cardLink:
+      'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
+  },
+  {
+    cardTitle: 'Иваново',
+    cardLink: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg',
+  },
+  {
+    cardTitle: 'Камчатка',
+    cardLink: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg',
+  },
+  {
+    cardTitle: 'Холмогорский район',
+    cardLink:
+      'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg',
+  },
+  {
+    cardTitle: 'Байкал',
+    cardLink: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
+  },
+]
 
-function popupOpener() {
-  inputName.value = profileName.textContent
-  inputJob.value = profileJob.textContent
+const templateCard = document.querySelector('#template-card').content
+const elements = document.querySelector('.elements')
+
+const profileButtonEdit = document.querySelector('.profile__button-edit')
+const profileName = document.querySelector('.profile__name-text')
+const profileJob = document.querySelector('.profile__job')
+const profilePopup = document.querySelector('.popup_name_profile')
+const profileForm = document.querySelector('.popup_name_profile .popup__form')
+const inputName = document.querySelector('.popup__input[name="name"]')
+const inputJob = document.querySelector('.popup__input[name="job"]')
+const profilePopupClose = document.querySelector('.popup_name_profile .popup__close-icon')
+
+const addCardButton = document.querySelector('.profile__button-plus')
+const addCardPopup = document.querySelector('.popup_name_add')
+const addCardForm = document.querySelector('.popup_name_add .popup__form')
+const inputTitle = document.querySelector('.popup__input[name="title"]')
+const inputLink = document.querySelector('.popup__input[name="link"]')
+const addCardPopupClose = document.querySelector('.popup_name_add .popup__close-icon')
+
+const fullImgPopup = document.querySelector('.popup_name_img')
+const fullImg = document.querySelector('.popup__img')
+const fullImgLabel = document.querySelector('.popup__img-label')
+const fullImgPopupClose = document.querySelector('.popup_name_img .popup__close-icon')
+
+function openPopup(popup) {
   popup.classList.add('popup_opened')
 }
 
-function popupCloser() {
+function closePopup(popup) {
   popup.classList.remove('popup_opened')
 }
 
-function formSubmitHandler(e) {
+function likeCard(e) {
+  e.target.classList.toggle('element__button-like_active')
+}
+
+function deleteCard(e) {
+  e.target.closest('.element').remove()
+}
+
+function openFullImg(e) {
+  const imgData = e.target
+  fullImgLabel.textContent = imgData.closest('.element').querySelector('.element__name').textContent
+  fullImg.src = imgData.src
+  fullImg.alt = imgData.alt
+  openPopup(fullImgPopup)
+}
+
+function addListeners(card) {
+  card.querySelector('.element__button-like').addEventListener('click', likeCard)
+  card.querySelector('.element__trash').addEventListener('click', deleteCard)
+  card.querySelector('.element__image').addEventListener('click', openFullImg)
+}
+
+function createCard(newCard) {
+  const card = templateCard.cloneNode(true),
+    cardImage = card.querySelector('.element__image')
+
+  card.querySelector('.element__name').textContent = newCard.cardTitle
+  cardImage.src = newCard.cardLink
+  cardImage.alt = newCard.cardTitle
+  addListeners(card)
+  return card
+}
+
+function renderCard(newCard) {
+  elements.prepend(createCard(newCard))
+}
+
+function renderInitialCards(initialCards) {
+  initialCards.forEach(renderCard)
+}
+
+renderInitialCards(initialCards)
+
+// profile
+profileButtonEdit.addEventListener('mousedown', () => {
+  inputName.value = profileName.textContent
+  inputJob.value = profileJob.textContent
+  openPopup(profilePopup)
+})
+
+profilePopupClose.addEventListener('click', () => {
+  closePopup(profilePopup)
+})
+
+profileForm.addEventListener('submit', (e) => {
   e.preventDefault()
   profileName.textContent = inputName.value
   profileJob.textContent = inputJob.value
-  popupCloser()
-}
+  closePopup(profilePopup)
+})
 
-profileButtonEdit.addEventListener('mousedown', popupOpener)
-popupClose.addEventListener('click', popupCloser)
-form.addEventListener('submit', formSubmitHandler)
+// addCard
+addCardButton.addEventListener('mousedown', () => {
+  openPopup(addCardPopup)
+})
 
-// popup.addEventListener('click', function (e) {
-//   if (e.target === e.currentTarget) {
-//     popupCloser()
-//   }
-// })
+addCardPopupClose.addEventListener('click', () => {
+  closePopup(addCardPopup)
+})
+
+addCardForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+  renderCard({ cardTitle: inputTitle.value, cardLink: inputLink.value })
+  closePopup(addCardPopup)
+})
+
+// fullImg
+fullImgPopupClose.addEventListener('click', () => {
+  closePopup(fullImgPopup)
+})
